ecbf8070bc2e86b0240a70f95d1236ed5ea190f7
Daniel Rogowski <daniel.rogowski@onet.pl>
2022-01-02 14:52:54 +0100

    Create files, that will represent Equation class

---
+++ equation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Daniel Rogowski on 02.01.2022.
+//
+
+#include "equation.h"
---
+++ equation.h
@@ -0,0 +1,14 @@
+//
+// Created by Daniel Rogowski on 02.01.2022.
+//
+
+#ifndef CONSOLE_CALC_EQUATION_H
+#define CONSOLE_CALC_EQUATION_H
+
+
+class equation {
+
+};
+
+
+#endif //CONSOLE_CALC_EQUATION_H
