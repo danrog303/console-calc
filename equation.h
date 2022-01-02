6508914e5f3af8b6352066c5917d3c0ccd6bf144
Daniel Rogowski <daniel.rogowski@onet.pl>
2022-01-02 14:57:37 +0100

    Add Equation class prototype to equation.h

--- equation.h
+++ equation.h
@@ -1,14 +1,19 @@
-//
-// Created by Daniel Rogowski on 02.01.2022.
-//
-
 #ifndef CONSOLE_CALC_EQUATION_H
 #define CONSOLE_CALC_EQUATION_H
+#include <stack>
+#include <sstream>
+#include <string>
+#include <utility>
 
-
-class equation {
-
+class Equation {
+private:
+    std::string equation;
+    static bool hasPrecedence(char op1, char op2);
+    static long double applyOperator(char op, long double a, long double b);
+public:
+    explicit Equation(std::string equation);
+    long double evaluate();
+    std::string getEquation();
 };
-
 
 #endif //CONSOLE_CALC_EQUATION_H
