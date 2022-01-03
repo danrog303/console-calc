68ed3eabd88de01fe20ab23ada692802939aa258
Czarkowski <cezarytytko@gmail.com>
2022-01-03 12:44:21 +0100

    Add implementation of main function

--- main.cpp
+++ main.cpp
@@ -3,7 +3,15 @@
 #include "help.h"
 #include "cli.h"
 
+using namespace std;
 int main(int argc, char** argv) {
-
+    if(argc >= 3) {
+        cout << "Invalid arguments count.\n";
+        cout << "Type console-calc --help to learn how to use console-calc.";
+    }
+    else if(argc == 2 && (string(argv[1]) == "--help" || string(argv[1]) == "-h")) {
+        help();
+    }
+	
 	return 0;
 }
