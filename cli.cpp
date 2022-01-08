1610a0e6ffdcbcf954850be369213d249c292538
bubo-py <kamil.chruscinski1@gmail.com>
2022-01-08 16:23:52 +0100

    Add a newline

--- cli.cpp
+++ cli.cpp
@@ -4,6 +4,7 @@
     std::cout << "Welcome to console-calc's CLI mode.\n";
     std::cout << "Please type any equation you want to calculate and press enter to get a result.\n";
     std::cout << "Press Ctrl+C to close the application.\n\n";
+
     do {
         std::cout << "> ";
         std::string line;
