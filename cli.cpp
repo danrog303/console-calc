c103f0088f8f6aaa3d40a12152e2c71ac48889e7
bubo-py <kamil.chruscinski1@gmail.com>
2022-01-08 16:21:45 +0100

    Update cli function

--- cli.cpp
+++ cli.cpp
@@ -1,5 +1,15 @@
 #include "cli.h"
 
 [[noreturn]] void cli() {
+    std::cout << "Welcome to console-calc's CLI mode.\n";
+    std::cout << "Please type any equation you want to calculate and press enter to get a result.\n";
+    std::cout << "Press Ctrl+C to close the application.\n\n";
+    do {
+        std::cout << "> ";
+        std::string line;
+        std::getline(std::cin, line);
 
+        Equation equation(line);
+        std::cout << equation.getEquation() << " = " << equation.evaluate() << "\n\n";
+    } while(true);
 }
