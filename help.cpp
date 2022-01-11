f4090aa48beee506f5c7d5ed2235085cf4576e00
Daniel Rogowski <daniel.rogowski@onet.pl>
2022-01-11 22:29:11 +0100

    Convert all cout usages to std::cout

--- help.cpp
+++ help.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 void help(){
-cout<<"***console-calc***\n";
-cout<<"This calculator uses endless loop to read the equations"<<endl;
-cout<<"console-calc\t\t\tenables CLI mode, which allows to calculate multiple equations in a loop\n\n";
-cout<<"\n\nUsage:\n";
-cout<<"It is capable of doing basic equations as addition, substraction, division and multiplication"<<endl;
-cout<<"console-calc \"(2+2)*2\"\tprints result of specified equation\n"<<endl;
-cout<<"You can learn more or submit issues on Gitbuh:\n";
-cout<<"https://github.com/danrog303/console-calc";
+std::cout<<"***console-calc***\n";
+std::cout<<"This calculator uses endless loop to read the equations"<<endl;
+std::cout<<"console-calc\t\t\tenables CLI mode, which allows to calculate multiple equations in a loop\n\n";
+std::cout<<"\n\nUsage:\n";
+std::cout<<"It is capable of doing basic equations as addition, substraction, division and multiplication"<<endl;
+std::cout<<"console-calc \"(2+2)*2\"\tprints result of specified equation\n"<<endl;
+std::cout<<"You can learn more or submit issues on Gitbuh:\n";
+std::cout<<"https://github.com/danrog303/console-calc";
 }
