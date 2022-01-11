4b43b20f327e73a95a27b6d073c422a63af42c01
Daniel Rogowski <daniel.rogowski@onet.pl>
2022-01-11 09:11:53 +0100

    Add code indentation in help.cpp

--- help.cpp
+++ help.cpp
@@ -1,13 +1,13 @@
 #include "help.h"
 using namespace std;
 
-void help(){
-cout<<"***console-calc***\n";
-cout<<"This calculator uses endless loop to read the equations"<<endl;
-cout<<"console-calc\t\t\tenables CLI mode, which allows to calculate multiple equations in a loop\n\n";
-cout<<"\n\nUsage:\n";
-cout<<"It is capable of doing basic equations as addition, substraction, division and multiplication"<<endl;
-cout<<"console-calc \"(2+2)*2\"\tprints result of specified equation\n"<<endl;
-cout<<"You can learn more or submit issues on Gitbuh:\n";
-cout<<"https://github.com/danrog303/console-calc";
+void help() {
+    cout<<"***console-calc***\n";
+    cout<<"This calculator uses endless loop to read the equations"<<endl;
+    cout<<"console-calc\t\t\tenables CLI mode, which allows to calculate multiple equations in a loop\n\n";
+    cout<<"\n\nUsage:\n";
+    cout<<"It is capable of doing basic equations as addition, substraction, division and multiplication"<<endl;
+    cout<<"console-calc \"(2+2)*2\"\tprints result of specified equation\n"<<endl;
+    cout<<"You can learn more or submit issues on Gitbuh:\n";
+    cout<<"https://github.com/danrog303/console-calc";
 }
