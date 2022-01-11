0d7d32d01128c271866b04ad3462142140d9c397
bubo-py <kamil.chruscinski1@gmail.com>
2022-01-11 21:45:32 +0100

    Add comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,23 @@
 
 using namespace std;
 int main(int argc, char** argv) {
+    // invalid arguments count
     if(argc >= 3) {
         cout << "Invalid arguments count.\n";
         cout << "Type console-calc --help to learn how to use console-calc.";
     }
+    // help message (-h)
     else if(argc == 2 && (string(argv[1]) == "--help" || string(argv[1]) == "-h")) {
         help();
     }
-	else if(argc == 1) {
+    // cli mode (no arguments)
+    else if(argc == 1) {
         cli();
     }
+    // calculate equation
     else if(argc == 2) {
         Equation eq(argv[1]);
         cout << eq.getEquation() << " = " << eq.evaluate();
     }
-	return 0;
+    return 0;
 }
